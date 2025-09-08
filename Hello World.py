c00979ee8cda1ef2e1d4fb24466a7e284a9e2cd1
Alfred Thelang <77567257+ALFRED-design110@users.noreply.github.com>
2025-09-08 11:32:34 +0200

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,11 @@
+# A simple program that displays or prints out 'Hello World'
+#print("Hello World")
+
+# A simple program that displays or prints out my name ('Alfred Thelang')
+#print('Alfred Thelang')
+
+#  A simple program that displays or prints out my home village
+#print('Maunatlala')
+
+# A simple program that displays or prints out my degree
+print("Business Intelligence and Data Analytics")
