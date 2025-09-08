1f540de1ad535d7cb35324b7d742d3ffcd85b329
Alfred Thelang <77567257+ALFRED-design110@users.noreply.github.com>
2025-09-08 11:19:35 +0200

    Add files via upload

---
+++ Even or Odd Checker.py
@@ -0,0 +1,9 @@
+''' A simple program that lets a user to input a number and checks if it's an even or 
+ a odd number '''
+
+number = int(input("Enter any number you want: "))
+
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Ödd number")
